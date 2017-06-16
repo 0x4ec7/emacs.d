5615cde178a507d112cd25db3247e672dedbda52
0x4ec7 <0x4ec7@gmail.com>
2017-06-16 14:27:20 +0800

    bind f12 to magit-status

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -3,6 +3,7 @@
 (eval-after-load 'magit
   '(progn
      (global-set-key (kbd "C-c g") 'magit-status)
+     (global-set-key (kbd "<f12>") 'magit-status)
      (nvmap :prefix "," "cg" 'magit-status)))
 
 
